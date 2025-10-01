a2599177f67a8d9c437a853e78bf58e1fa86e733
Reagan Frystak <fryst002@umn.edu>
2025-10-01 15:08:50 -0500

    times table with the use of the console AND HTML, includes button and generating the actual table

--- javascript.js
+++ javascript.js
@@ -1,56 +1,42 @@
+function getRowsDesired() {
+    // console.log(document.getElementById('rows').value);
+    // console.log(Number(document.querySelector('.rows input').value));
+    //console.log(Number(document.querySelectorAll('.rows input')[0].value));
+    //  console.log(Number(document.getElementById('rows').value));
 
-/*alert('adobe deleted my goddamn design file I worked all afternoon on')*/
+    return Number(document.getElementById("rows").value)
 
-console.log('Here\'s a for loop showing 1...10:\n')
-    for (let i=0; i<10; i++) {
-        console.log(`${i}\n'`)
-    }
-
-
-console.log('Here\'s a do-while loop showing 1...10:\n')
-let i=0
-do {
-    console.log(i+'\n')
-    i++
-} while (i < 10)
-
-
-console.log('Here\'s a while loop showing 1...10:\n')
-let j=0
-while (j < 10) {
-    console.log(`${i}\n'`)
-    j++
+    //most of these do the same thing it's just different ways to get the value i think
 }
 
+function getColumnsDesired() {
+    return Number(document.getElementById("cols").value)
+}
 
-console.log('Here\'s a foreach loop iterating over an array showing 1...20:\n')
-let nums=[...Array(20).keys()]
-nums.forEach(num=> {
-    console.log(`${ num }\n'`)
-})
+function createWithHtmlTable(rows, cols) {
+    let string
+    if (rows && cols) {
+        string = "<table\n"
+
+        for (let row = 1; row <= rows; row++) {
+            string += "<tr>\n"
 
 
-console.log('Here\'s a multiplication table method rendered in the console with a for loop:\n')
-let timesTable = (to) => {
-    console.log('Here\'s a times table to ' + to + '\n')
-    for (let i = 1; i <= to; i++) {
-        let row = ""
-        for (let j = 1; j <= to; j++) {
-            row += `${i * j}\t`
+            for (let col = 1; col <= cols; col++) {
+                string += "<td>"
+                string += row * col
+                string += "</td>\n"
+            }
+            string += "</tr>\n"
         }
-        console.log(row)
+
+        string += "</table>\n"
+
+    } else {
+        string = 'Provide some inputs'
     }
+
+    document.getElementById("output").innerHTML = string;
 }
 
-/* wtf did i just write */
-timesTable(9)
-timesTable(19)
-timesTable(6)
-
-
-let helloSomebody = (name) => {
-    console.log(`My name is ${name}\n`)
-}
-
-helloSomebody('Reagan')
-helloSomebody('Bob')
+getRowsDesired()
